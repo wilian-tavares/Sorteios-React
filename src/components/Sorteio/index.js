6fd6693c9f09dabf09acaa5a67a43d57ac38bef3
wilian-tavares <wilian.tavares@souunit.com.br>
2022-12-18 10:47:21 -0300

    layout do nome da apostas

--- src/components/Sorteio/index.js
+++ src/components/Sorteio/index.js
@@ -9,8 +9,11 @@ function Sorteio({ placeholder, funcao, id }){
     return(
         <div>        
             <div className="container-sorteio">
-              
-                <span id={id} className='campo-sorteio'>{placeholder}</span>
+                <div className='nameSorteio'>
+                    <span className='name'>{placeholder}</span>
+                    <span id={id} className='campo-sorteio'>{placeholder}</span>
+                </div>
+               
                 <button onClick={funcao} className='botao-sorteio'>SORTEAR</button>
                
             </div>
